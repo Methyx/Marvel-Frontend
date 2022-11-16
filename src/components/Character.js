e6d784829c349c9db3c2146523c95ab216a56be6
Philippe MERCY <philippemercy@macbook-pro-de-philippe-1.home>
2022-11-16 22:34:56 +0100

    little adjustments on characters cards v2

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,11 +3,11 @@ import "../css/character.css";
 const Character = ({ character }) => {
   return (
     <div className="character">
-      <p className="name"> {character.name}</p>
       <img
         src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
         alt="personnage"
       />
+      <p className="name"> {character.name}</p>
       <div className="description-container">
         <p className="description">{character.description}</p>
       </div>
