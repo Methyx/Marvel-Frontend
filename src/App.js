0802bf0b4fc422b72a71cb4d9b656521ee477a58
Philippe MERCY <philippemercy@macbook-pro-de-philippe-1.home>
2022-11-16 23:42:47 +0100

    search on characters

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Comics from "./pages/Comics";
 import Favoris from "./pages/Favoris";
+import Character from "./pages/Character";
 
 // components
 import Header from "./components/Header";
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/comics" element={<Comics />} />
         <Route path="/favoris" element={<Favoris />} />
+        <Route path="/character" element={<Character />} />
       </Routes>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,41 +3,82 @@ import axios from "axios";
 
 import "../css/home.css";
 
+// Components
 import Character from "../components/Character";
 
 const Home = () => {
   // STATES
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState(null);
+  const [searchCharacter, setSearchCharacter] = useState("");
 
-  // Initialisations
+  // functions
+  const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+    useEffect(() => {
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+      return () => {
+        clearTimeout(handler);
+      };
+    }, [value, delay]);
+    return debouncedValue;
+  };
 
+  // Init
+  const debounceSearchCharacter = useDebounce(searchCharacter, 500);
+
+  // UseEffect
   useEffect(() => {
     const fetchData = async () => {
-      const urlBack = "https://site--marvel-backend--gw6mlgwnmzwz.code.run";
+      const urlBack =
+        "https://site--marvel-backend--gw6mlgwnmzwz.code.run/characters";
+      let params = 0;
+      let url = urlBack;
       try {
-        const response = await axios.get(urlBack + "/characters");
+        if (debounceSearchCharacter) {
+          url += "?name=" + debounceSearchCharacter;
+          params++;
+        }
+        const response = await axios.get(url);
         setData(response.data);
         setIsLoading(false);
       } catch (error) {
         console.log(error.message);
       }
     };
+    // setIsLoading(true);
     fetchData();
-  }, []);
+  }, [debounceSearchCharacter]);
+
+  useEffect(() => {}, [searchCharacter]);
 
   return (
     <div className="container home-content">
       {isLoading ? (
         <p className="loading">Chargement en cours ...</p>
       ) : (
-        <div className="home">
-          <section className="characters-container">
-            {data.results.map((item) => {
-              return <Character key={item._id} character={item} />;
-            })}
-          </section>
-        </div>
+        <>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Chercher un personnage"
+              value={searchCharacter}
+              onChange={(event) => {
+                setSearchCharacter(event.target.value);
+              }}
+            />
+          </div>
+
+          <div className="home">
+            <section className="characters-container">
+              {data.results.map((item) => {
+                return <Character key={item._id} character={item} />;
+              })}
+            </section>
+          </div>
+        </>
       )}
     </div>
   );
