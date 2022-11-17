46002381da9ddf196860cf11b9931740e7f5127e
Philippe MERCY <philippemercy@macbook-pro-de-philippe-1.home>
2022-11-17 11:30:06 +0100

    page characters with hadle pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,8 +9,10 @@ import Character from "../components/Character";
 const Home = () => {
   // STATES
   const [isLoading, setIsLoading] = useState(true);
+  const [nbPerPage, setNbPerPage] = useState(100);
   const [data, setData] = useState(null);
   const [searchCharacter, setSearchCharacter] = useState("");
+  const [page, setPage] = useState(1);
 
   // functions
   const useDebounce = (value, delay) => {
@@ -18,6 +20,7 @@ const Home = () => {
     useEffect(() => {
       const handler = setTimeout(() => {
         setDebouncedValue(value);
+        setPage(1);
       }, delay);
       return () => {
         clearTimeout(handler);
@@ -28,19 +31,26 @@ const Home = () => {
 
   // Init
   const debounceSearchCharacter = useDebounce(searchCharacter, 500);
-
   // UseEffect
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const urlBack =
         "https://site--marvel-backend--gw6mlgwnmzwz.code.run/characters";
-      let params = 0;
       let url = urlBack;
       try {
-        if (debounceSearchCharacter) {
-          url += "?name=" + debounceSearchCharacter;
-          params++;
+        if (nbPerPage) {
+          url += "?limit=" + nbPerPage;
+        } else {
+          url += "?limit=100";
         }
+        if (page > 1) {
+          url += "&skip=" + Math.round((page - 1) * nbPerPage);
+        }
+        if (debounceSearchCharacter) {
+          url += "&name=" + debounceSearchCharacter;
+        }
+        // console.log(url);
         const response = await axios.get(url);
         setData(response.data);
         setIsLoading(false);
@@ -48,9 +58,8 @@ const Home = () => {
         console.log(error.message);
       }
     };
-    // setIsLoading(true);
     fetchData();
-  }, [debounceSearchCharacter]);
+  }, [debounceSearchCharacter, nbPerPage, page]);
 
   useEffect(() => {}, [searchCharacter]);
 
@@ -72,10 +81,40 @@ const Home = () => {
           </div>
 
           <div className="home">
+            <section className="handle-page-top">
+              <p>Personnages trouvés : {data.count}</p>
+              <div>
+                <span>Affichés par page : </span>
+                <input
+                  type="number"
+                  value={nbPerPage}
+                  onChange={(event) => {
+                    if (event.target.value <= 0) {
+                      setNbPerPage(0);
+                    } else if (event.target.value >= 100) {
+                      setNbPerPage(100);
+                    } else {
+                      setNbPerPage(event.target.value);
+                    }
+                  }}
+                />
+              </div>
+            </section>
             <section className="characters-container">
               {data.results.map((item) => {
                 return <Character key={item._id} character={item} />;
               })}
+            </section>
+            <section className="handle-page-bottom">
+              {page > 1 && (
+                <button onClick={() => setPage(page - 1)}> - </button>
+              )}
+              <span>
+                Page : {page} / {Math.ceil(data.count / nbPerPage)}
+              </span>
+              {page < Math.ceil(data.count / nbPerPage) && (
+                <button onClick={() => setPage(page + 1)}>+</button>
+              )}
             </section>
           </div>
         </>
